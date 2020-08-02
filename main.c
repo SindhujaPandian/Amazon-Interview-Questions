da7c94bf0fe11fa4cd314267b62dd789c70a151a
Sindhuja Pandian <51230035+SindhujaPandian@users.noreply.github.com>
2020-08-02 23:19:51 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,68 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct Node
-{
-    int data;
-    struct Node* next;
-};
-struct Node *head;
-struct Node *reverse( int k)
-{
-    struct Node* current = head;
-    struct Node* next = NULL;
-    struct Node* prev = NULL;
-    int count = 0;
-    while (current != NULL && count < k)
-    {
-        next  = current->next;
-        current->next = prev;
-        prev = current;
-        current = next;
-        count++;
-    }
 
-    if (next !=  NULL)
-       head->next = reverse(k);
-    return prev;
-}
-void printList(struct Node *node)
-{
-    while (node != NULL)
-    {
-        printf("%d  ", node->data);
-        node = node->next;
-    }
-}
-void insert(int val)
-{
-   struct Node* new_node =
-            (struct Node*) malloc(sizeof(struct Node));
-    struct Node * temp;
-    new_node->data  = val;
-
-    if(head==NULL)
-    {
-        new_node->next = NULL;
-        head = new_node;
-    }
-    else{
-            temp = head;
-    while (temp->next != NULL)
-    {
-        temp = temp->next;
-    }
-    temp->next = new_node;
-    new_node->next = NULL;
-    }
-}
 int main()
 {
+    int n,arr[100][100];
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+        for(int j=0; j<n; j++)
+            scanf("%d",&arr[i][j]);
+    int cycle = n/2;
+    for(int i=0;i<cycle;i++)
+        for(int j=i; j<(n-i-1);j++)
+    {
+        int temp = arr[i][j];
+        arr[i][j]=arr[j][n-i-1];
+        arr[j][n-i-1]=arr[n-i-1][n-j-1];
+        arr[n-i-1][n-j-1]=arr[n-j-1][i];
+        arr[n-j-1][i]=temp;
 
-    insert(1);
-    insert(2);
-    insert(3);
-    printList(head);
-    head = reverse(3);
-    printList(head);
+    }
+    for(int i=0; i<n; i++,printf("\n"))
+        for(int j=0;j<n;j++)
+            printf("%d ",arr[i][j]);
     return 0;
 }
